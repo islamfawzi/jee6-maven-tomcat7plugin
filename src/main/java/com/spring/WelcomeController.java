4096c322929f69a41b84e5a48b3e4c7d3bfbdf07
ifawzy <ifawzy.c@stc.com.sa>
2020-08-08 16:59:24 +0300

    add first spring mvc view using InternalResourceViewResolver

--- src/main/java/com/spring/WelcomeController.java
+++ src/main/java/com/spring/WelcomeController.java
@@ -1,18 +1,15 @@
 package com.spring;
 
-import javax.ws.rs.GET;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class WelcomeController {
 
 	@RequestMapping(method = RequestMethod.GET, value = "/")
-	@ResponseBody
+//	@ResponseBody
 	public String welcome() {
-		return "Welocme";
+		return "welcome";
 	}
 }
