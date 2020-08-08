9b266a3e2b1ede386b0da46a7162df0ef3d0cc36
ifawzy <ifawzy.c@stc.com.sa>
2020-08-08 18:06:23 +0300

    trying @RequestParam options

--- src/main/java/com/springmvc/WelcomeController.java
+++ src/main/java/com/springmvc/WelcomeController.java
@@ -1,5 +1,6 @@
 package com.springmvc;
 
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,9 +10,17 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class WelcomeController {
 
+	private Logger logger = Logger.getLogger(this.getClass());
+
 	@RequestMapping(method = RequestMethod.GET, value = "/")
 //	@ResponseBody
-	public String welcome(@RequestParam String name, ModelMap model) {
+
+//	@RequestParam(name="name") String name
+//	@RequestParam(defaultValue="defaultValue") String name
+//	@RequestParam Map<String,String> allParams  // ?id=1&name=islam
+//	@RequestParam List<String> ids    // ?ids=1,2,3,4
+	public String welcome(@RequestParam(required=false) String name, ModelMap model) {
+		logger.info("name: " + name);
 		model.put("name", name);
 		return "welcome";
 	}
